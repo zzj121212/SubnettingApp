84c1d573123049810be18605d89a509936ef49ba
Cliif Zhang <Zijia.Zhang@student.uts.edu.au>
2023-09-18 21:47:46 +1000

    Initial Commit

---
+++ SubnettingApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SubnettingApp
+//
+//  Created by apple on 18/9/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
